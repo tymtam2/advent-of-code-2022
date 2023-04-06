9fe8623ad08326fbd204bf0b0c172987d2107836
Tymek Majewski <tmaj@pm.me>
2023-04-06 21:46:14 +1000

    Day 1

---
+++ day01/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> l = new() { 0 };
+
+foreach (var line in File.ReadLines("input01.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        l.Add(0);
+    }
+    else
+    {
+        l[l.Count - 1] += int.Parse(line);
+    }
+}
+
+Console.WriteLine(l.Max());
+
+// Part 2
+l.Sort();
+l.Reverse();
+var top3 = l.Take(3);
+
+foreach(var t in top3)
+{
+    Console.WriteLine(t);
+}
+
+Console.WriteLine(top3.Sum());
